34371868da3d7b1a25260209f04d5e4a4eea8c1a
esterTion <lcz970@126.com>
2024-05-28 23:30:34 +0800

    修正ScoreGainOnScore类型

--- src/effect/types/ScoreGainOnScore.js
+++ src/effect/types/ScoreGainOnScore.js
@@ -2,16 +2,16 @@ import ConstText from "../../db/ConstText"
 
 export default class ScoreGainOnScore {
   static applyEffect(effect, calc, targets, type) {
-    if (effect.CalculationType !== 'Multiplication') throw new Error(`ScoreGainOnScore calc type: ${effect.CalculationType}`)
+    if (effect.CalculationType !== 'PercentageAddition') throw new Error(`ScoreGainOnScore calc type: ${effect.CalculationType}`)
     targets.forEach(idx => {
       if (!effect.conditionSatified(calc, idx)) return
       const liveSim = calc.liveSim
       const scoreRightNow = liveSim.baseScore * liveSim.currentTiming / liveSim.lastSenseTiming
-      + calc.result.senseScore.reduce((acc, cur) => acc + cur, 0)
-      + calc.result.starActScore.reduce((acc, cur) => acc + cur, 0)
-      const score = Math.floor(scoreRightNow * effect.activeEffect.Value / 100)
+        + calc.result.senseScore.reduce((acc, cur) => acc + cur, 0)
+        + calc.result.starActScore.reduce((acc, cur) => acc + cur, 0)
+      const score = Math.floor(scoreRightNow * effect.activeEffect.Value / 10000)
       calc.result.senseScore.push(score)
-      liveSim.phaseLog.push(ConstText.get('LIVE_LOG_POSTER_SCORE', [scoreRightNow, effect.activeEffect.Value / 100, score, 'score']))
+      liveSim.phaseLog.push(ConstText.get('LIVE_LOG_POSTER_SCORE', [scoreRightNow, (effect.activeEffect.Value / 100).toFixed(2) + '%', score, 'score']))
       root.addWarningMessage(ConstText.get('LOG_WARNING_INACCURATE_SCORE_GAIN_ON_SCORE'))
     })
   }
