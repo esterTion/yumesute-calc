18b37df36f06370d78948e99aab4267faaadf9df
esterTion <lcz970@126.com>
2024-07-31 00:10:41 +0800

    修复sa血量加成

--- src/effect/types/ScoreUpByHighLife.js
+++ src/effect/types/ScoreUpByHighLife.js
@@ -7,9 +7,9 @@ export default class ScoreUpByHighLife {
   static applyEffect(effect, calc, targets, type) {
     if (effect.CalculationType !== 'PercentageAddition') throw new Error(`ScoreUpByHighLife calc type: ${effect.CalculationType}`)
     const life = Math.min(calc.liveSim.life, ScoreUpByHighLife.LifeCap)
-    const bonus = Math.floor(0.01 * effect.activeEffect.Value * Math.pow(life / ScoreUpByHighLife.LifeCap, ScoreUpByHighLife.PowerValue)) / 100
-    const bonusLine = `${effect.activeEffect.Value / 100} × (${life} / ${ScoreUpByHighLife.LifeCap}) ^ ${ScoreUpByHighLife.PowerValue} = ${Math.round(bonus * 100)}`
     if (type === ScoreBonusType.Sense) {
+      const bonus = Math.floor(0.01 * effect.activeEffect.Value * Math.pow(life / ScoreUpByHighLife.LifeCap, ScoreUpByHighLife.PowerValue)) / 100
+      const bonusLine = `${effect.activeEffect.Value / 100} × (${life} / ${ScoreUpByHighLife.LifeCap}) ^ ${ScoreUpByHighLife.PowerValue} = ${Math.round(bonus * 100)}`
       calc.liveSim.activeBuff.sense.push({
         targets,
         effect,
@@ -20,6 +20,8 @@ export default class ScoreUpByHighLife {
       })
       calc.liveSim.phaseLog.push(ConstText.get('LIVE_LOG_SENSE_UP', [bonusLine, effect.DurationSecond]))
     } else if (type === ScoreBonusType.StarAct) {
+      const bonus = 0.01 * effect.activeEffect.Value * Math.pow(life / ScoreUpByHighLife.LifeCap, ScoreUpByHighLife.PowerValue) / 100
+      const bonusLine = `${effect.activeEffect.Value / 100} × (${life} / ${ScoreUpByHighLife.LifeCap}) ^ ${ScoreUpByHighLife.PowerValue} = ${bonus * 100}`
       calc.liveSim.activeBuff.starAct.push({
         targets,
         effect,
