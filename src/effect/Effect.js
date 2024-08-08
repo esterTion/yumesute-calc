7d5422010dd0c4b0325ef6f07cfc984e1e97b5e7
esterTion <lcz970@126.com>
2024-08-08 16:16:33 +0800

    修复p条条件判断

--- src/effect/Effect.js
+++ src/effect/Effect.js
@@ -126,7 +126,7 @@ export default class Effect {
       case 'DecreaseRequireSpecialLight': { return DecreaseRequireSpecialLight.applyEffect(this, calc, targets, type) }
       case 'LifeHealing': { return LifeHealing.applyEffect(this, calc, targets, type) }
       case "PrincipalGaugeUp": { return PrincipalGaugeUp.applyEffect(this, calc, targets, type) }
-      case 'PrincipalGaugeGain': { return PrincipalGaugeGain.applyEffect(this, calc, targets, type) }
+      case 'PrincipalGaugeGain': { return PrincipalGaugeGain.applyEffect(this, calc, [index], type) }
       case 'PrincipalGaugeLimitUp': { return PrincipalGaugeLimitUp.applyEffect(this, calc, targets, type) }
       case 'FinalPerformanceUpCancelSense': { return FinalPerformanceUpCancelSense.applyEffect(this, calc, targets, type) }
       case "VocalLimitUp": { return VocalLimitUp.applyEffect(this, calc, targets, type) }
--- src/effect/types/PrincipalGaugeGain.js
+++ src/effect/types/PrincipalGaugeGain.js
@@ -1,6 +1,9 @@
 export default class PrincipalGaugeGain {
   static applyEffect(effect, calc, targets, type) {
     if (effect.CalculationType !== 'FixedAddition') throw new Error(`PrincipalGaugeGain calc type: ${effect.CalculationType}`)
-    calc.liveSim.addPGauge(effect.activeEffect.Value)
+    targets.forEach(idx => {
+      if (!effect.conditionSatified(calc, idx)) return
+      calc.liveSim.addPGauge(effect.activeEffect.Value)
+    })
   }
 }
