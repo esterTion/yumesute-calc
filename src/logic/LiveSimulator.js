b9d4d0ca0b7334c5d7d85afd537c904acce4a378
esterTion <lcz970@126.com>
2024-08-08 17:05:26 +0800

    修改时间轴定位

--- src/logic/LiveSimulator.js
+++ src/logic/LiveSimulator.js
@@ -415,8 +415,8 @@ export default class LiveSimulator {
     this.calc.result.starActScore.push(score)
     this.calc.result.starActCount++
     this.phaseLog.push(ConstText.get('LIVE_LOG_STARACT_SCORE').replace('{0}', scoreLine))
-    const leftPixel = this.currentTiming ? 21 : 8
-    root.senseBox.children[0].children[1].appendChild(_('div', { className: 'staract-line', style: { left: `calc(${this.currentTiming/this.lastSenseTiming*100}% - ${leftPixel}px)` } }))
+    const leftStyle = this.currentTiming ? `calc(calc(calc(100% - 40px) * ${this.currentTiming/this.lastSenseTiming}) + 19px)` : '-8px'
+    root.senseBox.children[0].children[1].appendChild(_('div', { className: 'staract-line', style: { left: leftStyle } }))
     return true
   }
   purgeExpiredBuff(time) {
