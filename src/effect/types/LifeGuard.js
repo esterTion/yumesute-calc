d6d7af302ad8ddc6650b468500059626251d405d
esterTion <lcz970@126.com>
2025-04-27 00:30:23 +0800

    fes时雨技能故障应对

--- src/effect/types/LifeGuard.js
+++ src/effect/types/LifeGuard.js
@@ -2,5 +2,8 @@ export default class LifeGuard {
   static applyEffect(effect, calc, targets, type) {
     if (effect.CalculationType !== 'FixedAddition') throw new Error(`LifeGuard calc type: ${effect.CalculationType}`)
     calc.liveSim.lifeGuardCount += effect.activeEffect.Value
+    if (effect.data.Id === 29301) {
+      calc.liveSim.lifeGuardCount += effect.activeEffect.Value
+    }
   }
 }
