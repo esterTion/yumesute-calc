8cbd5a64e8e8a250efd092d4dbc8b171394e502a
esterTion <lcz970@126.com>
2025-07-21 11:37:40 +0800

    加盾sense还原

--- src/effect/types/LifeGuard.js
+++ src/effect/types/LifeGuard.js
@@ -2,8 +2,5 @@ export default class LifeGuard {
   static applyEffect(effect, calc, targets, type) {
     if (effect.CalculationType !== 'FixedAddition') throw new Error(`LifeGuard calc type: ${effect.CalculationType}`)
     calc.liveSim.lifeGuardCount += effect.activeEffect.Value
-    if (effect.data.Id === 29301) {
-      calc.liveSim.lifeGuardCount += effect.activeEffect.Value
-    }
   }
 }
