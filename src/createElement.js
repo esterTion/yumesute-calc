33079fdef599a13b106d9324a50483bbd638f4e3
esterTion <lcz970@126.com>
2025-08-04 09:37:41 +0800

    createElement支持创建fragment

--- src/createElement.js
+++ src/createElement.js
@@ -5,4 +5,50 @@
 * @param {HTMLElement[]} children
 * @returns HTMLElement
 */
-export default function _(e,t,i){var a=null;if("text"===e)return document.createTextNode(t);a=document.createElement(e);for(var n in t)if("style"===n)for(var o in t.style)a.style[o]=t.style[o];else if("className"===n)a.className=t[n];else if("event"===n)for(var p in t.event)a.addEventListener(p,t.event[p]);else a.setAttribute(n,t[n]);if(i)if("string"==typeof i)a.innerHTML=i;else if(Array.isArray(i))for(var l=0;l<i.length;l++)null!=i[l]&&a.appendChild(i[l]);return a}
+export default function _(name, textOrAttr, childs) {
+  var element = null;
+  if ("text" === name) {
+    return document.createTextNode(textOrAttr);
+  }
+  switch (name) {
+    case CREATE_FRAGMENT: {
+      element = document.createDocumentFragment();
+      break;
+    }
+    default: {
+      element = document.createElement(name);
+    }
+  }
+  for (var attr in textOrAttr) {
+    if ("style" === attr) {
+      for (var style in textOrAttr.style) {
+        element.style[style] = textOrAttr.style[style];
+      }
+    }
+    else if ("className" === attr) {
+      element.className = textOrAttr[attr];
+    }
+    else if ("event" === attr) {
+      for (var event in textOrAttr.event) {
+        element.addEventListener(event, textOrAttr.event[event]);
+      }
+    }
+    else {
+      element.setAttribute(attr, textOrAttr[attr]);
+    }
+  }
+  if (childs) {
+    if ("string" == typeof childs) {
+      element.innerHTML = childs;
+    } else if (Array.isArray(childs)) {
+      for (var l = 0; l < childs.length; l++) {
+        if (null != childs[l]) {
+          element.appendChild(childs[l]);
+        }
+      }
+    }
+  }
+  return element
+}
+
+export const CREATE_FRAGMENT = Symbol('fragment_element_marker');
