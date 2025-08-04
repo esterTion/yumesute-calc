8bbd46c73c454def2091c023d6e6edaa08676a3a
esterTion <lcz970@126.com>
2025-08-04 21:44:45 +0800

    修正sa分数加分的计算时机

--- src/effect/types/ScoreGainOnScore.js
+++ src/effect/types/ScoreGainOnScore.js
@@ -16,8 +16,15 @@ export default class ScoreGainOnScore {
         liveSim.phaseLog.push(ConstText.get('LIVE_LOG_POSTER_SCORE', [scoreRightNow, (effect.activeEffect.Value / 100).toFixed(2) + '%', score, 'score']))
         root.addWarningMessage(ConstText.get('LOG_WARNING_INACCURATE_SCORE_GAIN_ON_SCORE'))
       } else if (type === ScoreBonusType.StarAct) {
-        calc.result.senseScore.push(score)
-        liveSim.phaseLog.push(ConstText.get('LIVE_LOG_STARACT_SCORE_EXTRA', [scoreRightNow, (effect.activeEffect.Value / 100).toFixed(2) + '%', score, 'score']))
+        // sa分数百分比加分操作在sa算分后
+        calc.liveSim.pendingActions.push(() => {
+          const scoreRightNow = liveSim.baseScore * liveSim.currentTiming / liveSim.lastSenseTiming
+            + calc.result.senseScore.reduce((acc, cur) => acc + cur, 0)
+            + calc.result.starActScore.reduce((acc, cur) => acc + cur, 0)
+          const score = Math.floor(scoreRightNow * effect.activeEffect.Value / 10000)
+          calc.result.starActScore.push(score)
+          liveSim.phaseLog.push(ConstText.get('LIVE_LOG_STARACT_SCORE_EXTRA', [scoreRightNow, (effect.activeEffect.Value / 100).toFixed(2) + '%', score, 'score']))
+        })
         root.addWarningMessage(ConstText.get('LOG_WARNING_INACCURATE_SCORE_GAIN_ON_SCORE'))
       } else {
         root.addWarningMessage(ConstText.get('LOG_WARNING_EFFECT_SCORE_GAIN_TYPE_NOT_IMPLEMENTED', {type: type, id: effect.Id}))
