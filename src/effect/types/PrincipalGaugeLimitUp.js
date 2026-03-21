0918ef05d1900c277dffa0dce2f44eae381b0e33
esterTion <lcz970@126.com>
2026-03-22 01:06:01 +0800

    游戏bug适配 （【新たなる王、翠玉の戴冠】阿岐留カミラ：无视PercentageAddition）

--- src/effect/types/PrincipalGaugeLimitUp.js
+++ src/effect/types/PrincipalGaugeLimitUp.js
@@ -1,6 +1,8 @@
 export default class PrincipalGaugeLimitUp {
   static applyEffect(effect, calc, targets, type) {
-    if (effect.CalculationType !== 'FixedAddition') throw new Error(`PrincipalGaugeLimitUp calc type: ${effect.CalculationType}`)
+    // 【新たなる王、翠玉の戴冠】阿岐留カミラ
+    // 无视计算类型，一概判断为加
+    // if (effect.CalculationType !== 'FixedAddition') throw new Error(`PrincipalGaugeLimitUp calc type: ${effect.CalculationType}`)
     targets.forEach(idx => {
       if (!effect.conditionSatified(calc, idx)) return
       calc.liveSim.addPGaugeLimit(effect.activeEffect.Value)
